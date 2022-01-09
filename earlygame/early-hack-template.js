defd794b3352fd4b01aae70ecac97178eb754b11
Mike Tupker <tupcakes@users.noreply.github.com>
2022-01-09 08:36:45 -0600

    test

--- earlygame/early-hack-template.js
+++ earlygame/early-hack-template.js
@@ -3,21 +3,24 @@ export async function main(ns) {
     let target = ns.args[0];
     ns.disableLog('ALL');
     ns.clearLog();
+
+    let moneyThresh = ns.getServerMaxMoney(target) * 0.75;
+    let securityThresh = ns.getServerMinSecurityLevel(target) + 5;
     
     while (true) {
-        if (ns.getServerSecurityLevel(target) > ns.getServerMinSecurityLevel(target)) {
+        if (ns.getServerSecurityLevel(target) > securityThresh) {
             ns.print("");
             ns.print("Running weaken");
             ns.print("");
             await ns.weaken(target);
         }
-        if (ns.getServerMoneyAvailable(target) < ns.getServerMaxMoney(target)) {
+        if (ns.getServerMoneyAvailable(target) < moneyThresh) {
             ns.print("");
             ns.print("Running grow");
             ns.print("");
             await ns.grow(target);
         }
-        if (ns.getServerSecurityLevel(target) > ns.getServerMinSecurityLevel(target)) {
+        if (ns.getServerSecurityLevel(target) > securityThresh) {
             ns.print("");
             ns.print("Running 2nd weaken");
             ns.print("");

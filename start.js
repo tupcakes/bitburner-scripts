f00a030ce5aa21c658031cc9df25adf97de4a99c
Mike Tupker <tupcakes@users.noreply.github.com>
2022-01-10 12:45:58 -0600

    added other noram servers

--- start.js
+++ start.js
@@ -14,6 +14,7 @@ export async function main(ns) {
 	if (ns.getServerMaxRam(ns.getHostname()) >= 512) {
 		if (ns.getHackingLevel() >= 250) {
 			ns.run("control-home.js", 1, "silver-helix");
+			ns.run("control-home.js", 1, "max-hardware");
 		}
 	}
 
@@ -23,10 +24,16 @@ export async function main(ns) {
 			ns.run("control-home.js", 1, "crush-fitness");
 		}
 	}
+	if (ns.getServerMaxRam(ns.getHostname()) >= 2048) {
+		if (ns.getHackingLevel() >= 1000) {
+			ns.run("control-home.js", 1, "syscore");
+			ns.run("control-home.js", 1, "snap-fitness");
+		}
+	}
 
 	// only run max of 2 here
 	if (ns.getPurchasedServers().length > 0) {
-		let server = "max-hardware";
+		let server = "crush-fitness";
 		if (ns.getServerRequiredHackingLevel(server) >= ns.getHackingLevel()) {
 			ns.run("control.js", 1, server);
 		}

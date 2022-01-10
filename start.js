a0bdd7c25c93a42cc9ffd766cdab08e368091105
Mike Tupker <tupcakes@users.noreply.github.com>
2022-01-10 12:35:16 -0600

    logic for level and ram limits

--- start.js
+++ start.js
@@ -5,18 +5,35 @@ export async function main(ns) {
 	ns.run("/earlygame/attack_local.js");
 	ns.run("control-home.js", 1, "n00dles");
 
-	// if home ram >= 128 - add later
-	if (ns.getHackingLevel() >= 100) {
-		ns.run("control-home.js", 1, "joesguns");
+	if (ns.getServerMaxRam(ns.getHostname()) >= 128) {
+		if (ns.getHackingLevel() >= 100) {
+			ns.run("control-home.js", 1, "joesguns");
+		}
 	}
-	// if home ram >= 512 - add later
-	if (ns.getHackingLevel() >= 250) {
-		ns.run("control-home.js", 1, "silver-helix");
+
+	if (ns.getServerMaxRam(ns.getHostname()) >= 512) {
+		if (ns.getHackingLevel() >= 250) {
+			ns.run("control-home.js", 1, "silver-helix");
+		}
+	}
+
+	if (ns.getServerMaxRam(ns.getHostname()) >= 1024) {
+		if (ns.getHackingLevel() >= 500) {
+			ns.run("control-home.js", 1, "comptek");
+			ns.run("control-home.js", 1, "crush-fitness");
+		}
 	}
 
 	// only run max of 2 here
-	if (ns.getPurchasedServers().length > 0){
-		ns.run("control.js", 1, "max-hardware");
-		ns.run("control.js", 1, "johnson-ortho");
+	if (ns.getPurchasedServers().length > 0) {
+		let server = "max-hardware";
+		if (ns.getServerRequiredHackingLevel(server) >= ns.getHackingLevel()) {
+			ns.run("control.js", 1, server);
+		}
+		
+		server = "johnson-ortho";
+		if (ns.getServerRequiredHackingLevel(server) >= ns.getHackingLevel()) {
+			ns.run("control.js", 1, server);
+		}
 	}
 }

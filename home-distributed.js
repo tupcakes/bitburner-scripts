1d86c15db2e844863d27b30b764424e5883db897
Mike Tupker <tupcakes@users.noreply.github.com>
2022-01-11 16:46:10 -0600

    new control

---
+++ home-distributed.js
@@ -0,0 +1,22 @@
+/** @param {NS} ns **/
+export async function main(ns) {
+	// get all pservs
+	let pserv = ns.getPurchasedServers();
+	// read in server file
+	let file = ns.read("server_list.txt");
+	let servers = file.split("\r\n");
+
+	for (let i = 0; i < servers.length; ++i) {
+		let server = JSON.stringify(servers[i].split(",")).replace('["', '').replace('"]', '');
+
+		let hackinglevel = ns.getHackingLevel();
+		let ServerRequiredHackingLevel = ns.getServerRequiredHackingLevel(server);
+		let ServerMaxMoney = ns.getServerMaxMoney(server);
+
+		if (ServerRequiredHackingLevel <= hackinglevel && ServerMaxMoney > 0) {
+			ns.exec("control.js", 'home', 1, server);
+		} else {
+			ns.tprint("Server " + server + " has too high of a security level or has no money. Skipping.");
+		}
+	}
+}

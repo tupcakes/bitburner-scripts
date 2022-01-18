53c4e668a7d7f1c18edbc2ef6ae144b5257e9f33
Mike Tupker <tupcakes@users.noreply.github.com>
2022-01-18 10:56:31 -0600

    update

--- singularity_scripts/crime.js
+++ singularity_scripts/crime.js
@@ -1,9 +1,18 @@
 /** @param {NS} ns **/
 export async function main(ns) {
-	ns.stopAction();
-	while (true) {
-		ns.commitCrime('Homicide');
+	let loop = true
 
+	// makes the script require just over 32 GB ram
+	// document.addEventListener('keypress', ({ key }) => {
+	// 	if (key === 'Enter') {
+	// 		loop = false
+	// 	}
+	// })
+
+	ns.stopAction();
+	while (loop) {
+		ns.commitCrime('Homicide');
+		ns.print(ns.heart.break());
 		await ns.sleep(3500);
 	}
 }

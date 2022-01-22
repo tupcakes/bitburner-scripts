bd32378ee0a3dfd5a690bf290cab3aa08e3f4aba
Mike Tupker <tupcakes@users.noreply.github.com>
2022-01-22 10:42:04 -0600

    only start needed stuff

--- start.js
+++ start.js
@@ -1,28 +1,8 @@
 /** @param {NS} ns **/
 export async function main(ns) {
-	let pserv = ns.getPurchasedServers();
-
 	ns.run("mcp.js");
-	ns.run("earlygame/attack_local.js");
+	ns.run("/gangs/tasks.js");
 
 	await ns.sleep(10000);
-	ns.run("control.js", 1, "n00dles");
-
-	if (pserv.length > 0) {
-		ns.run("control.js", 1, "foodnstuff");
-
-		if (ns.getHackingLevel() >= 100) {
-			ns.run("control.js", 1, "joesguns");
-			ns.run("control.js", 1, "max-hardware");
-			ns.run("control.js", 1, "sigma-cosmetics");
-			ns.run("control.js", 1, "hong-fang-tea");
-			ns.run("control.js", 1, "harakiri-sushi");
-			ns.run("control.js", 1, "iron-gym");
-		}
-		if (ns.getHackingLevel() >= 200) {
-			ns.run("control.js", 1, "zer0");
-			ns.run("control.js", 1, "max-hardware");
-			ns.run("control.js", 1, "silver-helix");
-		}
-	}
+	//ns.run("coordinator.js", 1, "n00dles");
 }

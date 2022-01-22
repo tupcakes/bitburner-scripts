30aa7fa1aed827160ef36e391fe52f357c4dab13
Mike Tupker <tupcakes@users.noreply.github.com>
2022-01-22 11:11:52 -0600

    always run the right amount of control.js

---
+++ pserv-controller.js
@@ -0,0 +1,30 @@
+/**
+ * control.js is setup to allow running about two instances per pserv.
+ * this will run the max possible - 1 instances of control.js based
+ * on the number of available pservs.
+ */
+
+/** @param {NS} ns **/
+export async function main(ns) {
+	let serverlist = ns.read("server_list.txt");
+	let targets = serverlist.split("\r\n");
+
+
+	while (true) {
+		await ns.sleep(20);
+
+		let pservs = ns.getPurchasedServers();
+		let maxtargets = (pservs.length * 2) - 1;
+
+		for (let i = 0; i < targets.length; ++i) {
+			let target = JSON.stringify(targets[i].split(",")).replace('["', '').replace('"]', '');
+			if (ns.getServerRequiredHackingLevel(target) <= ns.getHackingLevel() && ns.hasRootAccess(target) === true) {
+				ns.run("control.js", 1, target);
+			}
+
+			if (i > maxtargets) {
+				break;
+			}
+		}
+	}
+}
--- start.js
+++ start.js
@@ -2,7 +2,5 @@
 export async function main(ns) {
 	ns.run("mcp.js");
 	ns.run("/gangs/tasks.js");
-
-	await ns.sleep(10000);
-	//ns.run("coordinator.js", 1, "n00dles");
+	ns.run("pserv-controller.js");
 }

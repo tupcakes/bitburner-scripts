e30d8c33df6c813fffc03f2a2f5746bd18b03435
Mike Tupker <tupcakes@users.noreply.github.com>
2022-01-22 20:51:34 -0600

    updated pservs on each loop

--- upgradepservs.js
+++ upgradepservs.js
@@ -1,10 +1,11 @@
 /** @param {NS} ns **/
 export async function main(ns) {
 	let moneytokeep = 1000000000;
-	let pservs = ns.getPurchasedServers();
 
 	while (true) {
 		await ns.sleep(20);
+
+		let pservs = ns.getPurchasedServers();
 
 		if (pservs.length === 24) {
 			for (const pserv of pservs) {

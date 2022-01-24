ffa0af33ab4596ae0b3777fe28e1d726e58dde02
Mike Tupker <tupcakes@users.noreply.github.com>
2022-01-24 10:01:40 -0600

    upgrade servers once

--- buy/upgradepservs.js
+++ buy/upgradepservs.js
@@ -1,24 +1,28 @@
 /** @param {NS} ns **/
 export async function main(ns) {
-	let moneytokeep = 1000000000;
+	let pservs = ns.getPurchasedServers();
+	if (pservs.length !== 25) {
+		ns.tprint("You don't have 25 servers.");
+		return;
+	}
 
 	while (true) {
 		await ns.sleep(20);
 
-		let pservs = ns.getPurchasedServers();
-
-		if (pservs.length === 25) {
-			for (const pserv of pservs) {
-				let currentserverram = ns.getServerMaxRam(pserv);
-
-				if (ns.getServerMoneyAvailable('home') > moneytokeep) {
-					ns.killall(pserv);
-					ns.deleteServer(pserv);
-
-					let newram = currentserverram + currentserverram
-					let index = pserv.slice(-1);
-					ns.purchaseServer("pserv-" + newram + "GB-" + index, newram);
-				}
+		for (let i = 0; i < pservs.length; ++i) {
+			let currentserverram = ns.getServerMaxRam(pservs[i]);
+			// if ram of last server is greater than ram of current, then upgrade
+			if (ns.getServerMaxRam(pservs[i - 1]) > currentserverram) {
+				ns.killall(pservs[i]);
+				ns.deleteServer(pservs[i]);
+				let newram = currentserverram + currentserverram
+				ns.purchaseServer("pserv-" + i + "-" + newram + "GB", newram);
+			}
+			
+			// check if last server has been upgraded, quit if true
+			if (ns.getServerMaxRam(pservs.length) === newram) {
+				ns.tprint("All servers upgraded to " + newram);
+				return;
 			}
 		}
 	}

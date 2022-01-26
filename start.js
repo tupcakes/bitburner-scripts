1834a3c03925e4f637e92355e37861e86dbfb014
Mike Tupker <tupcakes@users.noreply.github.com>
2022-01-26 11:47:38 -0600

    update

--- start.js
+++ start.js
@@ -1,4 +1,4 @@
-import { updatefiles, getportopeners } from "/libraries/utils.js";
+import { updatefiles, getportopeners, getmostprofitable } from "/libraries/utils.js";
 
 /** @param {NS} ns **/
 export async function main(ns) {
@@ -13,15 +13,17 @@ export async function main(ns) {
 	ns.run("mcp.js");
 	if (ns.gang.inGang() === true) {
 		ns.run("/gangs/tasks.js");
+		ns.run("distributed-share.js");
 	}
-	ns.run("distributed-share.js");
-	// ns.run("upgradepservs.js");
-
+	
 	if (pservs.length > 0 && pservs.length < 25) {
 		ns.run("pserv-controller.js");
 	} else if (pservs.length === 25) {
 		ns.scriptKill('pserv-controller.js', 'home');
 		ns.run("home-distributed.js");
+	} else {
+		let bestserver = getmostprofitable(ns);
+		ns.run('/earlygame/coordinator.js', 1, bestserver);
 	}
 
 	if (ns.getHackingLevel() >= ns.getServerRequiredHackingLevel('megacorp')) {

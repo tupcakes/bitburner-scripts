207ef3d280601a70b9968af0e6afb44a4b88407f
Mike Tupker <tupcakes@users.noreply.github.com>
2022-01-29 09:38:50 -0600

    rewrote hacknet upgrader

--- buy/hacknet.js
+++ buy/hacknet.js
@@ -1,108 +1,51 @@
-let functions = { buyNode, upgradeNodes, buyNodes, help };
-let maxNodeCost;
-/*
- * Time to get some Hacknet Nodes!
- * You can call this script with the options
- * buyNode - purchase and upgrade one new node
- * upgradeNodes - upgrade your existing network to max
- * buyNodes - buy as many nodes as you can afford
- * help - help with the command
- */
+/** @param {NS} ns **/
 export async function main(ns) {
-    let result;
-    if (!Object.keys(functions).includes(ns.args[0])) {
-        result = 'Not Found';
-        help(ns);
-    } else {
-        result = functions[ns.args[0]](ns);
-    }
-    response(ns, ns.args[0], result);
-}
+	ns.disableLog('ALL');
+	//ns.enableLog('exec');
+	ns.clearLog();
+	ns.tail();
+	
+	let nodelimit = 9;
+	let allupgraded = false;
 
-function help(ns) {
-    ns.tprint("This script helps you manage your Hacknet Nodes. " +
-        "You can purchase new nodes by running `run buyHacknet.js buyNode` " +
-        "or `run buyHacknet.js buyNodes`. It will purchase as many nodes as you can afford. " +
-        "You can automatically upgrade your existing hacknet nodes by running " +
-        "`run buyHacknet.js upgradeNodes`");
-    return '';
-}
+	let nodecount = ns.hacknet.numNodes();
 
-function response(ns, func, result) {
-    switch (func) {
-        case 'buyNode':
-            ns.tprint(result ? '✔️Purchased one node' : 'You cannot afford a node.😢');
-            break;
-        case 'buyNodes':
-            ns.tprint(result > 0 ? `✔️Purchased ${result} nodes.` : 'You cannot afford a node.😢');
-            break;
-        case 'upgradeNodes':
-            ns.tprint(result ? '✔️Nodes Upgraded' : '⭐You are already upgraded as much as you can afford.');
-            break;
-        default:
-            if (result === 'Not Found') { ns.tprint(`${func} is not a valid option`); }
-    }
-}
 
-function buyNode(ns) {
-    let index = ns.hacknet.purchaseNode();
-    if (index < 0) {
-        return false;
-    }
-    maxNodeCost = maxNodeCost || getMaxNodeCost(ns, index);
+	while (allupgraded === false) {
+		await ns.sleep(20);
+		// buy nodes up to our limit
+		while (nodecount < nodelimit) {
+			await ns.sleep(20);
+			ns.hacknet.purchaseNode();
+			nodecount = ns.hacknet.numNodes();
+		}
 
-    if (ns.getServerMoneyAvailable('home') >= maxNodeCost) {
-        ns.hacknet.upgradeLevel(index, 200);
-        ns.hacknet.upgradeRam(index, 6);
-        ns.hacknet.upgradeCore(index, 16);
-    } else {
-        upgradeNode(ns, index);
-    }
-    return true;
-}
+		// build array of nodes and their stats
+		let nodes = [];
+		for (let i = 0; i < nodecount; ++i) {
+			let stats = ns.hacknet.getNodeStats(i);
+			const node = new Object
+			node.index = nodecount[i];
+			node.cores = stats.cores;
+			node.ram = stats.ram;
+			node.level = stats.level;
+			nodes.push(node);
+		}
 
-function buyNodes(ns) {
-    let purchaseNodes = true;
-    let i = 0;
-    while (purchaseNodes) {
-        purchaseNodes = buyNode(ns);
-        if (purchaseNodes) { i++; }
-    }
-    return i;
-}
-
-function getMaxNodeCost(ns, index) {
-    ns.tprint('Fetching cost');
-    return ns.hacknet.getLevelUpgradeCost(index, 200) +
-        ns.hacknet.getRamUpgradeCost(index, 6) +
-        ns.hacknet.getCoreUpgradeCost(index, 16);
-}
-
-function upgradeNodes(ns) {
-    let upgrade = false;
-    let nodes = ns.hacknet.numNodes();
-    for (let i = 0; i < nodes; i++) {
-        upgrade = upgradeNode(ns, i) || upgrade;
-    }
-    return upgrade;
-}
-
-function upgradeNode(ns, index) {
-    let level = upgradeNodePart(ns, index, 'Level', 10);
-    let node = upgradeNodePart(ns, index, 'Ram', 2);
-    let core = upgradeNodePart(ns, index, 'Core', 1);
-    return level || node || core;
-}
-
-function upgradeNodePart(ns, nodeIndex, upgradePart, increment) {
-    let upgrade = false;
-    let costFunction = `get${upgradePart}UpgradeCost`;
-    let upgradeFunction = `upgrade${upgradePart}`;
-    let cost = ns.hacknet[costFunction](nodeIndex, increment);
-    while (isFinite(cost) && ns.getServerMoneyAvailable('home') >= cost) {
-        ns.hacknet[upgradeFunction](nodeIndex, increment);
-        cost = ns.hacknet[costFunction](nodeIndex, increment);
-        upgrade = true;
-    }
-    return upgrade;
+		// if node stats aren't at max attempt upgrade
+		for (let i = 0; i < nodes.length; ++i) {
+			if (nodes[i].cores < 16) {// cores - max 16
+				ns.hacknet.upgradeCore(i, 1);
+			}
+			if (nodes[i].ram < 64) { // ram - max 64
+				ns.hacknet.upgradeRam(i, 1);
+			}
+			if (nodes[i].level < 200) { // level - max 200
+				ns.hacknet.upgradeLevel(i, 1);
+			}
+			if (nodes[nodelimit - 1].cores === 16 && nodes[nodelimit - 1].ram === 64 && nodes[nodelimit - 1].level === 200) {
+				allupgraded = true;
+			}
+		}
+	}
 }

f0ab56425a1cc422d18d829ff8d0d1c1a6ed1d3d
Mike Tupker <tupcakes@users.noreply.github.com>
2022-01-30 16:23:37 -0600

    new metric

--- hudstart.js
+++ hudstart.js
@@ -2,33 +2,36 @@ import { getpservcount, getpservram, getrootedcount, getrootedram, controlscript
 
 /** @param {NS} ns **/
 export async function main(ns) {
-  ns.tail();
-  ns.disableLog('ALL');
+	ns.tail();
+	ns.disableLog('ALL');
 
 
-  while (true) {
-    ns.print("-----Available Server Resources-----")
-    ns.print("Total pservs:                " + getpservcount(ns));
-    ns.print("Pserv 0 ram:                 " + getpservram(ns));
-    ns.print("Total hacked servers:        " + getrootedcount(ns));
-    ns.print("Total hacked servers ram:    " + getrootedram(ns) + " GB");
-    ns.print("");
-    ns.print("-----Running Processes-----")
-    ns.print("MCP running:                 " + mcprunning(ns));
-    ns.print("Gang control running:        " + gangsrunning(ns));
-    ns.print("Pserv control running:       " + pservcontrollerrunning(ns));
-    ns.print("Share running:               " + distsharerunning(ns));
-    ns.print("Control scripts running:     " + controlscriptsrunning(ns));
-    ns.print("Coordinator scripts running: " + coordinatorscriptsrunning(ns));
-    ns.print("Cheeseint running:           " + cheeseintrunning(ns));
-    ns.print("");
-    ns.print("-----Gang Info-----")
-    ns.print("Wanted Level:                " + ns.gang.getGangInformation().wantedLevel.toFixed(2));
-    ns.print("Wanted Rate:                 " + ns.gang.getGangInformation().wantedLevelGainRate.toFixed(2));
-    ns.print("Territory:                   " + ((ns.gang.getGangInformation().territory) * 100).toFixed(2));
+	while (true) {
+		let homeramavailable = ns.getServerMaxRam('home') - ns.getServerUsedRam('home');
 
-    ns.print(Math.floor(Math.random() * 1000));
-    await ns.sleep(20);
-    ns.clearLog();
-  }
+		ns.print("-----Available Server Resources-----")
+		ns.print("Home ram available/total:    " + homeramavailable + "/" + ns.getServerMaxRam('home') + " GB");
+		ns.print("Total pservs:                " + getpservcount(ns));
+		ns.print("Pserv 0 ram:                 " + getpservram(ns) + " GB");
+		ns.print("Total hacked servers:        " + getrootedcount(ns));
+		ns.print("Total hacked servers ram:    " + getrootedram(ns) + " GB");
+		ns.print("");
+		ns.print("-----Running Processes-----")
+		ns.print("MCP running:                 " + mcprunning(ns));
+		ns.print("Gang control running:        " + gangsrunning(ns));
+		ns.print("Pserv control running:       " + pservcontrollerrunning(ns));
+		ns.print("Share running:               " + distsharerunning(ns));
+		ns.print("Control scripts running:     " + controlscriptsrunning(ns));
+		ns.print("Coordinator scripts running: " + coordinatorscriptsrunning(ns));
+		ns.print("Cheeseint running:           " + cheeseintrunning(ns));
+		ns.print("");
+		ns.print("-----Gang Info-----")
+		ns.print("Wanted Level:                " + ns.gang.getGangInformation().wantedLevel.toFixed(2));
+		ns.print("Wanted Rate:                 " + ns.gang.getGangInformation().wantedLevelGainRate.toFixed(2));
+		ns.print("Territory:                   " + ((ns.gang.getGangInformation().territory) * 100).toFixed(2));
+
+		ns.print(Math.floor(Math.random() * 1000));
+		await ns.sleep(20);
+		ns.clearLog();
+	}
 }

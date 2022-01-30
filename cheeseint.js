9ecfd1bb321cff765e1568684178510e1ad58177
Mike Tupker <tupcakes@users.noreply.github.com>
2022-01-30 11:51:03 -0600

    update

--- cheeseint.js
+++ cheeseint.js
@@ -1,11 +1,26 @@
+//import { createexes } from "/libraries/utils.js";
+
 /** @param {NS} ns **/
 export async function main(ns) {
+	ns.disableLog('ALL');
+	//ns.enableLog('exec');
+	ns.clearLog();
 	ns.tail();
+	
 	const programs = [
-		"DeepscanV1.exe",
-		"DeepscanV2.exe",
+		//"DeepscanV1.exe",
+		//"DeepscanV2.exe",
 		"AutoLink.exe",
-		"ServerProfiler.exe",
+		//"ServerProfiler.exe",
+	];
+
+	const cities = [
+		"Aevum",
+		"Volhaven",
+		"Chongqing",
+		"New Tokyo",
+		"Ishima",
+		"Sector-12",
 	];
 
 	while (true) {
@@ -13,9 +28,14 @@ export async function main(ns) {
 
 		if (ns.isBusy() === false) {
 			let randomprogram = programs[Math.floor(Math.random() * programs.length)];
+			ns.print("Removing: " + randomprogram);
 			ns.rm(randomprogram);
 		}
 
-		
+		for (const program of programs) {
+			if (ns.isBusy() === false) {
+				ns.createProgram(program, false);
+			}
+		}
 	}
 }

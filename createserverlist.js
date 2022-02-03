3fa234d59df13c6b0c27dfc87fe3e20ee304e4d2
Mike Tupker <tupcakes@users.noreply.github.com>
2022-02-03 10:25:29 -0600

    filtered out npo servers

--- createserverlist.js
+++ createserverlist.js
@@ -4,7 +4,11 @@ import { spider } from "/libraries/spider.js";
 export async function main(ns) {
     let servers = spider(ns);
 
-    servers.sort((a, b) => a.hackinglevel - b.hackinglevel);
-    
-    await ns.write('serversbyhacklvl.json', JSON.stringify(servers), "a");
+    const filteredservers = servers.filter(element => {
+        return element.name.includes('pserv-') === false && element.name.includes('darkweb') === false && element.name.includes('home') === false;
+    });
+
+    filteredservers.sort((a, b) => a.hackinglevel - b.hackinglevel);
+
+    await ns.write('serversbyhacklvl.json', JSON.stringify(filteredservers), "w");
 }

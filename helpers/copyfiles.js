45fe7fff4ae2966180a6d4962cf76876639f3b9c
Mike Tupker <tupcakes@users.noreply.github.com>
2022-02-05 09:50:03 -0600

    updated to include json list

--- helpers/copyfiles.js
+++ helpers/copyfiles.js
@@ -3,7 +3,7 @@ export async function main(ns) {
     let target = ns.args[0];
 
     let files = ns.ls('home', ".js");
-	files.push('server_list.txt');
+	files.push('serversbyhacklvl.json.txt');
 
     for (let i = 0; i < files.length; i++) {
         ns.print(target + ": " + files[i]);

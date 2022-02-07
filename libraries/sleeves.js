0ff08e6c931c84e09fefddedc9e294d39d03d704
Mike Tupker <tupcakes@users.noreply.github.com>
2022-02-07 09:42:10 -0600

    sleeves

---
+++ libraries/sleeves.js
@@ -0,0 +1,65 @@
+/** @param {NS} ns **/
+export function getsleevecrimechance(ns, crimearg, sleevenumber) {
+    let crime = ns.getCrimeStats(crimearg);
+    let sleeveinfo = ns.sleeve.getInformation(sleevenumber);
+    let sleeve = ns.sleeve.getSleeveStats(sleevenumber);
+
+    let chance =
+        crime.hacking_success_weight * sleeve.hacking +
+        crime.strength_success_weight * sleeve.strength +
+        crime.defense_success_weight * sleeve.defense +
+        crime.dexterity_success_weight * sleeve.dexterity +
+        crime.agility_success_weight * sleeve.agility +
+        crime.charisma_success_weight * sleeve.charisma;
+        //0.025 * sleeve.intelligence;  // int crime weight
+    chance /= 975;  // maxskilllevel 975
+    chance /= crime.difficulty;
+    chance *= sleeveinfo.mult.crimeSuccess;  // this is reference by mult.crimeSuccess
+    //chance *= calculateIntelligenceBonus(sleeve.intelligence);
+
+    return Math.min(chance, 1);
+}
+
+
+/** @param {NS} ns **/
+export function getcrimerisk(ns, sleevenumber) {
+    const crimes = [
+        "Shoplift",
+        "RobStore",
+        "Mug",
+        "Larceny",
+        "Drugs",
+        "BondForgery",
+        "TraffickArms",
+        "Homicide",
+        "GrandTheftAuto",
+        "Kidnap",
+        "Assassination",
+        "Heist",
+    ];
+
+    /** Calculate the risk value of all crimes */
+    let choices = crimes.map((crime) => {
+        let crimeStats = ns.getCrimeStats(crime); // Let us look at the important bits
+        let crimeChance = getsleevecrimechance(ns, crime, sleevenumber); // We need to calculate if its worth it
+        /** Using probabilty(odds) to calculate the "risk" to get the best reward
+        * Risk Value = Money Earned * Odds of Success(P(A) / ~P(A)) / Time taken
+        *
+        * Larger risk values indicate a better choice
+        */
+        let crimeRiskValue =
+            (crimeStats.money * Math.log10(crimeChance / (1 - crimeChance + Number.EPSILON))) /
+            crimeStats.time;
+        return [crime, crimeRiskValue];
+    });
+    return choices;
+}
+
+
+/** @param {NS} ns **/
+export function getbestcrime(ns, choices) {
+    let bestCrime = choices.reduce((prev, current) => {
+        return prev[1] > current[1] ? prev : current;
+    });
+    return bestCrime;
+}
---
+++ sleeves/sleeve-control.js
@@ -0,0 +1,44 @@
+import { getcrimerisk, getbestcrime, getsleevecrimechance } from "/libraries/sleeves.js";
+
+
+/** @param {NS} ns **/
+export async function main(ns) {
+	while (true) {
+		await ns.sleep(20);
+
+		let sleeves = ns.sleeve.getNumSleeves();
+
+		// // recover
+		// for (let i = 0; i < sleeves; i++) {
+		// 	if (ns.sleeve.getSleeveStats(sleeves[i]).shock > 0) {
+		// 		ns.sleeve.setToShockRecovery(sleeves[i]);
+		// 	}
+		// }
+
+		// // commit crimes
+		// for (let i = 0; i < sleeves; i++) {
+		// 	if (ns.sleeve.getSleeveStats(sleeves[i]).shock === 0) {
+		// 		ns.sleeve.setToCommitCrime(sleeves[i], 'Mug');
+		// 		//ns.sleeve.setToCommitCrime(sleeves[i], 'Homicide');
+		// 	}
+		// }
+
+		// commit crimes
+		for (let i = 0; i < sleeves; i++) {
+			let task = ns.sleeve.getTask(0);
+			if (ns.gang.inGang() === false) {
+				if (getsleevecrimechance(ns, 'Homicide', i) < .9 && task.crime !== 'Mug') {
+					ns.sleeve.setToCommitCrime(sleeves[i], 'Mug');
+				} else if (getsleevecrimechance(ns, 'Homicide', i) >= .9 && task.crime !== 'Homicide') {
+					ns.sleeve.setToCommitCrime(sleeves[i], 'Homicide');
+				}
+			} else {
+				let choices = getcrimerisk(ns, i);
+				let bestcrime = getbestcrime(ns, choices);
+				if (task.crime !== bestcrime) {
+					ns.sleeve.setToCommitCrime(sleeves[i], bestcrime);
+				}
+			}
+		}
+	}
+}

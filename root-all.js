b10dcc9fbf97e8cfc5a04c058a76d10cd79a9a00
Mike Tupker <tupcakes@users.noreply.github.com>
2022-02-08 13:59:43 -0600

    update

--- root-all.js
+++ root-all.js
@@ -1,14 +1,68 @@
+import { countPrograms } from "/libraries/root.js";
+
 /** @param {NS} ns **/
 export async function main(ns) {
-	let servers = JSON.parse(ns.read("serversbyhacklvl.json.txt"));
+	ns.disableLog('ALL');
+	ns.clearLog();
+
+	const factionservers = [
+		"CSEC",
+		"avmnite-02h",
+		"I.I.I.I",
+		"run4theh111z",
+	];
+
+	let targets = JSON.parse(ns.read("serversbyhacklvl.json.txt"));
+
+	let pservs = ns.getPurchasedServers();
+	let allservers = [];
+	allservers.push("home");
+	for (const pserv of pservs) {
+		allservers.push(pserv);
+	}
+	for (const target of targets) {
+		if (ns.getServerMaxRam(target.name) > 0) {
+			allservers.push(target.name);
+		}
+	}
+
+
 	while (true) {
-		await ns.sleep(5000);
-		for (let i = 0; i < servers.length; ++i) {
-			if (ns.getServerRequiredHackingLevel(servers[i].name) <= ns.getHackingLevel()) {
-				// check if it already has root access, if not nuke it
-				if (ns.hasRootAccess(servers[i].name) == false) {
-					ns.print("Getting root.");
-					ns.run("/helpers/get_root.js", 1, servers[i].name);
+		await ns.sleep(100);
+		roottargetsloop:
+		for (let i = 0; i < targets.length; ++i) {
+			if (ns.hasRootAccess(targets[i].name) === true) {
+				await ns.sleep(20);
+				continue roottargetsloop;
+			}
+			if (ns.getHackingLevel() >= ns.getServerRequiredHackingLevel(targets[i].name)) {
+				rootallserversloop:
+				for (let j = 0; j < allservers.length; ++j) {
+					if (countPrograms(ns) >= ns.getServerNumPortsRequired(targets[i].name)) {
+						let availableram = ns.getServerMaxRam(allservers[j]) - ns.getServerUsedRam(allservers[j]);
+						if (availableram > get_root_ram) {
+							ns.exec('/helpers/get_root.js', allservers[j], 1, targets[i].name)
+							ns.tprint("Rooted: " + targets[i].name);
+							ns.print("Rooted: " + targets[i].name);
+
+							let copyfiles_pid = ns.run('/helpers/copyfiles.js', 1, targets[i].name);
+							await ns.sleep(100);
+
+							copyrunningloop:
+							while (ns.isRunning(copyfiles_pid) === true) {
+								await ns.sleep(20);
+								continue copyrunningloop;
+							}
+							// backdoor
+							if (factionservers.includes(targets[i].name)) {
+								ns.run('/helpers/backdoor.js', 1, targets[i].name);
+								ns.tprint("Backdoored: " + targets[i].name);
+								ns.print("Backdoored: " + targets[i].name);
+								await ns.sleep(5000);
+							}
+							break rootallserversloop;
+						}
+					}
 				}
 			}
 		}

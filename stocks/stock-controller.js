483ac4934647d87f3286e52118693d2ab9d1d8b7
Mike Tupker <tupcakes@users.noreply.github.com>
2022-02-09 16:30:26 -0600

    update

--- stocks/stock-controller.js
+++ stocks/stock-controller.js
@@ -4,11 +4,11 @@ export async function main(ns) {
 		await ns.sleep(1000);
 		ns.run('/stocks/dash-stocks.js');
 		ns.run('/stocks/pricetracker.js');
-		ns.run('/stocks/seller.js');
 		//ns.run('/stocks/manipulate.js');
 
 		// wait for pricetracker to generate some data
 		await ns.sleep(120000);
+		ns.run('/stocks/seller.js');
 		ns.run('/stocks/buyer.js');
 	}
 }

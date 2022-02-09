436c0f56528782600505945445b20e1dd8977886
Mike Tupker <tupcakes@users.noreply.github.com>
2022-02-09 12:39:06 -0600

    added sell price

--- stocks/dash-stocks.js
+++ stocks/dash-stocks.js
@@ -3,6 +3,12 @@ export async function main(ns) {
 	ns.tail();
 	ns.disableLog('ALL');
 
+	let dollarUS = Intl.NumberFormat("en-US", {
+		style: "currency",
+		currency: "USD",
+		maximumFractionDigits: 0,
+	});
+
 	while (true) {
 		await ns.sleep(1000);
 		ns.clearLog();
@@ -10,11 +16,13 @@ export async function main(ns) {
 
 		let symbols = ns.stock.getSymbols();
 
-		ns.print("Sym-----Shares-----AvgPrc-----CntPrc")
+		ns.print("Sym-----Shares-----AvgPrc-----CntPrc------SellPrc");
 
 		for (const sym of symbols) {
-			if (ns.stock.getPosition(sym)[0] !== 0) {
-				ns.print(sym + "     " + ns.stock.getPosition(sym)[0] + "       " + ns.stock.getPosition(sym)[1].toFixed(2) + "    " + ns.stock.getPrice(sym).toFixed(2));
+			let position = ns.stock.getPosition(sym);
+			if (position[0] !== 0) {
+				let sellprice = position[1] + (position[1] * .25);
+				ns.print(sym + "     " + ns.stock.getPosition(sym)[0] + "       " + ns.stock.getPosition(sym)[1].toFixed(2) + "    " + ns.stock.getPrice(sym).toFixed(2) + "      " + sellprice.toFixed(2));
 			}
 		}
 		ns.print(Math.floor(Math.random() * 1000));

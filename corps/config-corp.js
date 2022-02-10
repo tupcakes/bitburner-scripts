ec3be5094b4ec602bd0c90f66e9b7f588f269a4c
Mike Tupker <tupcakes@users.noreply.github.com>
2022-02-10 10:38:12 -0600

    automate hiring employees when available space

--- corps/config-corp.js
+++ corps/config-corp.js
@@ -13,6 +13,15 @@ export async function main(ns) {
 	// assign employees to jobs
 	for (const division of divisions) {
 		for (const city of cities) {
+			// get total possible employees for the city
+			let officesize = ns.corporation.getOffice(division.name, city).size;
+			let currentemployeecount = ns.corporation.getOffice(division.name, city).employees.length;
+			let needtohire = officesize - currentemployeecount;
+		
+			for (let i = 0; i < needtohire; i++) {
+				ns.corporation.hireEmployee(division.name, city);
+			}
+
 			let employees = await getemployees(ns, division.name, city);
 			await assignemployees(ns, division.name, city, employees);
 		}

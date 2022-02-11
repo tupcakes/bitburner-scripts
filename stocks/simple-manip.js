4fa1dc241e1eaefd34e7c7a99cb7600036bcd8fa
Mike Tupker <tupcakes@users.noreply.github.com>
2022-02-11 16:46:16 -0600

    not working yet.

--- stocks/simple-manip.js
+++ stocks/simple-manip.js
@@ -19,7 +19,7 @@ export async function main(ns) {
 			// if we own the stock grow it
 			if (position[0] !== 0) {
 				if (ns.hasRootAccess(targets[i].server) === true) {
-					//await raiseprice(ns, targets[i].server, targets[i].sym)
+					ns.kill('/stocks/simple-weaken-stock.js', 'home', targets[i].server, targets[i].sym)
 					ns.run('/stocks/simple-grow-stock.js', 1, targets[i].server, targets[i].sym);
 				}
 			}
@@ -27,7 +27,7 @@ export async function main(ns) {
 			// if we don't own the stock weaken it
 			if (position[0] === 0) {
 				if (ns.hasRootAccess(targets[i].server) === true) {
-					//await lowerprice(ns, targets[i].server, targets[i].sym)
+					ns.kill('/stocks/simple-grow-stock.js', 'home', targets[i].server, targets[i].sym)
 					ns.run('/stocks/simple-weaken-stock.js', 1, targets[i].server, targets[i].sym);
 				}
 			}

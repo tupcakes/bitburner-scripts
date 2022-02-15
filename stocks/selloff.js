fdcf720d3bfd1708ec7ddd0c92409da9d1eba4ce
Mike Tupker <tupcakes@users.noreply.github.com>
2022-02-15 09:17:48 -0600

    shorts

--- stocks/selloff.js
+++ stocks/selloff.js
@@ -1,9 +1,11 @@
 /** @param {NS} ns **/
 export async function main(ns) {
 	let symbols = ns.stock.getSymbols();
+	let total = 0;
 
 	for (const sym of symbols) {
 		let position = ns.stock.getPosition(sym);
 		ns.stock.sell(sym, position[0]);
+		ns.stock.sellShort(sym, position[2]);
 	}
 }

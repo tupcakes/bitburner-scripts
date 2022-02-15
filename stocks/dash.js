8d7016cbca2584b491cd16cbd1e00032ee129f77
Mike Tupker <tupcakes@users.noreply.github.com>
2022-02-15 09:31:29 -0600

    better info

--- stocks/dash.js
+++ stocks/dash.js
@@ -16,7 +16,13 @@ export async function main(ns) {
 		ns.clearLog();
 
 		let totalinvested = 0;
-		let totalworth = 0;
+		let totalinvworth = 0;
+
+		let totalinvestedlong = 0;
+		let totalinvestedshort = 0;
+
+		let totalinvworthlong = 0;
+		let totalinvworthshort = 0;
 		let prctchange = 0;
 
 		let symbols = ns.stock.getSymbols();
@@ -24,22 +30,35 @@ export async function main(ns) {
 		for (const sym of symbols) {
 			let position = ns.stock.getPosition(sym);
 			if (position[0] !== 0) {
-				totalinvested = totalinvested + (position[0] * position[1]);
-				//totalworth = totalworth + (ns.stock.getBidPrice(sym) * ns.stock.getPosition(sym)[0]);
-				totalworth = totalworth + ns.stock.getSaleGain(sym, position[0], 'Long');
+				totalinvestedlong = totalinvestedlong + (position[0] * position[1]);
+				totalinvworthlong = totalinvworthlong + ns.stock.getSaleGain(sym, position[0], 'Long');
 			} else if (position[2] != 0) {
-				totalinvested = totalinvested + (position[2] * position[3]);
-				totalworth = totalworth + ns.stock.getSaleGain(sym, position[2], 'Short');
+				totalinvestedshort = totalinvestedshort + (position[2] * position[3]);
+				totalinvworthshort = totalinvworthshort + ns.stock.getSaleGain(sym, position[2], 'Short');
 			}
 		}
 
-		prctchange = ((totalworth + ns.getServerMoneyAvailable('home') - startingmoney) / startingmoney) * 100;
+		totalinvested = totalinvestedlong + totalinvestedshort;
+		totalinvworth = totalinvworthlong + totalinvworthshort;
 
+
+		prctchange = ((totalinvworth + ns.getServerMoneyAvailable('home') - startingmoney) / startingmoney) * 100;
+
+		ns.print("-----TOTAL-----");
 		ns.print("Staring Money    " + dollarUS.format(startingmoney));
 		ns.print("Total Inv:       " + dollarUS.format(totalinvested));
-		ns.print("Total Inv Worth: " + dollarUS.format(totalworth));
-		ns.print("Total Assets:    " + dollarUS.format(totalworth + ns.getServerMoneyAvailable('home')));
+		ns.print("Total Inv Worth: " + dollarUS.format(totalinvworth));
 		ns.print("% Change:        " + prctchange.toFixed(2));
+
+		ns.print("-----LONG-----");
+		ns.print("Total Inv:       " + dollarUS.format(totalinvestedlong));
+		ns.print("Total Inv Worth: " + dollarUS.format(totalinvworthlong));
+		ns.print("Total Assets:    " + dollarUS.format(totalinvworthlong + ns.getServerMoneyAvailable('home')));
+		
+		ns.print("-----SHORT-----");
+		ns.print("Total Inv:       " + dollarUS.format(totalinvestedshort));
+		ns.print("Total Inv Worth: " + dollarUS.format(totalinvworthshort));
+		ns.print("Total Assets:    " + dollarUS.format(totalinvworthshort + ns.getServerMoneyAvailable('home')));
 
 		ns.print(Math.floor(Math.random() * 1000));
 	}

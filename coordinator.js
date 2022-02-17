406ce1f4e0dbc2f2880552536d5d3f77350f6224
Mike Tupker <tupcakes@users.noreply.github.com>
2022-02-17 11:21:50 -0600

    new coordinator

--- coordinator.js
+++ coordinator.js
@@ -1,265 +1,179 @@
-/**
- * This is a early to mid level script. it will assign hack, weaken,
- * and grow jobs to rooted servers, then psers, then home.
- */
-
-
- export function autocomplete(data, args) {
-	return data.servers;
+/** @param {NS} ns **/
+export function getgrowthreads(ns, target) {
+	let growmoneyuntilmax = ns.getServerMaxMoney(target) - ns.getServerMoneyAvailable(target);
+	if (growmoneyuntilmax === 0) {
+		return -1;
+	}
+	return Math.ceil(ns.growthAnalyze(target, growmoneyuntilmax));
 }
 
 /** @param {NS} ns **/
-export async function main(ns) {
-	let target = ns.args[0];
+export function gethackthreads(ns, target, hackmultiplier) {
+	if (hackmultiplier > 1) {
+		ns.tprint("Hack multiplier must be less than 1.");
+		return -1;
+	}
+	return Math.ceil((ns.getServerMaxMoney(target) * hackmultiplier) / ns.hackAnalyze(target));
+}
 
+/** @param {NS} ns **/
+export function getweakenthreads(ns, target) {
+	if ((ns.getServerSecurityLevel(target) - ns.getServerMinSecurityLevel(target)) === 0) {
+		return 0;
+	} else {
+		return Math.ceil((ns.getServerSecurityLevel(target) - ns.getServerMinSecurityLevel(target)) / ns.weakenAnalyze(1));
+	}
+}
+
+/** @param {NS} ns **/
+export function getfreeram(ns, target) {
+	return ns.getServerMaxRam(target) - ns.getServerUsedRam(target);
+}
+
+/** @param {NS} ns **/
+export function showstats(ns, target, usableservers, operation, threadsrequired, threadsremaining) {
 	let dollarUS = Intl.NumberFormat("en-US", {
 		style: "currency",
 		currency: "USD",
 		maximumFractionDigits: 0,
 	});
 
+	ns.clearLog();
+	ns.print("Running:                " + operation);
+	ns.print("ServerMoneyAvailable:   " + dollarUS.format(ns.getServerMoneyAvailable(target)));
+	ns.print("ServerMaxMoney:         " + dollarUS.format(ns.getServerMaxMoney(target)));
+	ns.print("ServerSecurityLevel:    " + ns.getServerSecurityLevel(target));
+	ns.print("ServerMinSecurityLevel: " + ns.getServerMinSecurityLevel(target));
+	ns.print("Usable servers length:  " + usableservers.length);
+	ns.print("Threads required:       " + threadsrequired);
+	ns.print("Threads remaining:      " + threadsremaining);
+	ns.print(Math.floor(Math.random() * 1000));
+}
+
+export function autocomplete(data, args) {
+	return data.servers;
+}
+
+/** @param {NS} ns **/
+export async function main(ns) {
+	ns.tail();
 	ns.disableLog('ALL');
-	//ns.enableLog('exec');
+	// ns.enableLog('exec');
 	ns.clearLog();
 
-	// multipliers - ADJUST THIS
-	let weakenmultiplier = .2;
-	let growmultiplier = 1.15;
-	let moneymultiplier = .20;
-
-
-	let sleepoffset = 2000;
-	let hacktime = 0;
-	let weakentime = 0;
-	let growtime = 0;
+	let target = ns.args[0];
 
 	let weakenscriptram = ns.getScriptRam('/helpers/weaken1.js');
 	let growscriptram = ns.getScriptRam('/helpers/grow.js');
 	let hackscriptram = ns.getScriptRam('/helpers/hack.js');
-	let scriptram = weakenscriptram + growscriptram + hackscriptram;
 
-	let usableserversthreadsavailable = 0;
-
-	let firstweakenrunning = false;
-	let growrunning = false;
-	let secondweakenrunning = false;
-	let usableserversfreeram = 0;
-	let pid = null;
-
-
+	mainloop:
 	while (true) {
-		await ns.sleep(20);
+		await ns.sleep(50);
 
-		// build list of usable servers
-		let allservers = JSON.parse(ns.read("serversbyhacklvl.json.txt"));
-		let usableservers = [];
+		let hackmultiplier = .75;
 
-		let pservs = ns.getPurchasedServers();
-		// add all rootable servers that have ram and we have root on
-		for (const server of allservers) {
-			if (ns.getServerMaxRam(server.name) > 0 && ns.hasRootAccess(server.name)) {
-				usableservers.push(server.name);
-			}
-		}
-		// for (const pserv of pservs) {
-		// 	usableservers.push(pserv);
-		// }
-		//usableservers.push("home");
-		
-		// get ram of all usableservers
-		let totalram = 0;
-		for (const usableserver of usableservers) {
-			totalram = totalram + ns.getServerMaxRam(usableserver);
-		}
-		// subtrack room for one instance of each script as a buffer.
-		let maxnumthreads = Math.floor((totalram - scriptram) / scriptram);
+		// update server list
+		ns.run('createserverlist.js');
+		let usableservers = JSON.parse(ns.read("serversbyhacklvl.json.txt"));
 
+		let weakenthreadsrequired = getweakenthreads(ns, target);
+		let weakenthreadsremaining = weakenthreadsrequired;
+		let growthreadsrequired = getgrowthreads(ns, target);
+		let growthreadsremaining = growthreadsrequired;
+		let hackthreadsrequired = gethackthreads(ns, target, hackmultiplier);
+		let hackthreadsremaining = hackthreadsrequired;
+		let sleeptime = 0;
 
-		let reqhackthreads = Math.max(1, ns.hackAnalyzeThreads(target, (ns.getServerMaxMoney(target) * moneymultiplier)));
-		// hackthreads returned a value less than 1
-		if (target == 'n00dles') {
-			reqhackthreads = Math.max(1, ns.hackAnalyzeThreads(target, (ns.getServerMaxMoney(target) - 70000)));
-		}
-		let moneyperhack = (ns.getServerMaxMoney(target) * ns.hackAnalyze(target)) * reqhackthreads;
-		let remaininghackthreads = Math.ceil(reqhackthreads);
-
-
-		ns.clearLog();
-		ns.print("ServerMoneyAvailable:   " + dollarUS.format(ns.getServerMoneyAvailable(target)));
-		ns.print("ServerMaxMoney:         " + dollarUS.format(ns.getServerMaxMoney(target)));
-		ns.print("Money per hack cycle:   " + dollarUS.format(moneyperhack));
-		ns.print("ServerSecurityLevel:    " + ns.getServerSecurityLevel(target));
-		ns.print("ServerMinSecurityLevel: " + ns.getServerMinSecurityLevel(target));
-		ns.print("Usable servers length:  " + usableservers.length);
-		ns.print("Hack threads:           " + reqhackthreads);
-		// visual test to see if it's still looping
-		ns.print(Math.floor(Math.random() * 1000));
-
-
-		if (ns.getServerMoneyAvailable(target) === ns.getServerMaxMoney(target) && ns.getServerSecurityLevel(target) === ns.getServerMinSecurityLevel(target)) {
-			usableserversthreadsavailable = 0;
-			hacktime = ns.getHackTime(target);
-			for (let i = 0; i < usableservers.length; ++i) {
+		// WEAKEN
+		if (ns.getServerSecurityLevel(target) > ns.getServerMinSecurityLevel(target)) {
+			threadloop:
+			while (weakenthreadsremaining > 0) {
+				showstats(ns, target, usableservers, "Weaken", weakenthreadsrequired, weakenthreadsremaining)
 				await ns.sleep(20);
-
-				// get usable ram and determine 
-				usableserversfreeram = ns.getServerMaxRam(usableservers[i]) - ns.getServerUsedRam(usableservers[i]);
-				usableserversthreadsavailable = Math.ceil(usableserversfreeram / hackscriptram) - 2;
-				if (usableserversthreadsavailable === 0) {
-					continue;
-				}
-				if (remaininghackthreads === 0) {
-					break;
-				}
-
-				ns.clearLog();
-				ns.print("------HACK RUNNING------");
-				ns.print("Running hack for        " + hacktime + " ms");
-				ns.print("Thread available:       " + usableserversthreadsavailable);
-				ns.print("Current security:       " + ns.getServerSecurityLevel(target));
-				ns.print("Money available:        " + dollarUS.format(ns.getServerMoneyAvailable(target)));
-				ns.print("Money to hack:          " + dollarUS.format(moneyperhack));
-
-				// if remaining threads is less than the host can run, do the last exec. otherwise run with max available
-				// threads.
-				if (usableserversthreadsavailable > remaininghackthreads) {
-					pid = ns.exec("/helpers/hack.js", usableservers[i], remaininghackthreads, target, hacktime);
-					break;
-				} else {
-					pid = ns.exec("/helpers/hack.js", usableservers[i], usableserversthreadsavailable, target, 0);
-					remaininghackthreads = remaininghackthreads - usableserversthreadsavailable;
-				}
-
-				// if batch is running go to the next host
-				if (ns.isRunning(pid, usableservers[i], usableserversthreadsavailable, target, 0) === false) {
-					continue;
-				}
-			}
-			await ns.sleep(hacktime);
-		} else {
-			// set the threads
-			let reqweakenthreads = Math.ceil(maxnumthreads * weakenmultiplier);
-			let reqgrowthreads = Math.ceil(maxnumthreads * growmultiplier);
-
-
-
-			let remainingfirstweakenthreads = Math.ceil(reqweakenthreads);
-			let remaininggrowthreads = Math.ceil(reqgrowthreads);
-			let remainingsecondweakenthreads = Math.ceil(reqweakenthreads);
-
-
-			// first weaken
-			if (firstweakenrunning === false && growrunning === false && secondweakenrunning === false) {
-				usableserversthreadsavailable = 0;
+				usableserversloop:
 				for (let i = 0; i < usableservers.length; ++i) {
 					await ns.sleep(20);
-
-					// get usable ram and determine 
-					usableserversfreeram = ns.getServerMaxRam(usableservers[i]) - ns.getServerUsedRam(usableservers[i]);
-					usableserversthreadsavailable = Math.ceil(usableserversfreeram / weakenscriptram) - 2;
-					if (usableserversthreadsavailable === 0) {
-						continue;
+					let freeram = getfreeram(ns, usableservers[i].name);
+					if (freeram < weakenscriptram) {
+						continue usableserversloop;
 					}
-					if (remainingfirstweakenthreads === 0) {
-						break;
-					}
+					let maxthreadsonhost = Math.floor(freeram / weakenscriptram);
 
-					// if remaining threads is less than the host can run, do the last exec. otherwise run with max available
-					// threads.
-					if (usableserversthreadsavailable > remainingfirstweakenthreads) {
-						pid = ns.exec("/helpers/weaken1.js", usableservers[i], remainingfirstweakenthreads, target, hacktime);
-						weakentime = ns.getWeakenTime(target) + sleepoffset;
-						//break;
-					} else {
-						pid = ns.exec("/helpers/weaken1.js", usableservers[i], usableserversthreadsavailable, target, hacktime);
-						remainingfirstweakenthreads = remainingfirstweakenthreads - usableserversthreadsavailable;
-					}
-
-					// if batch is running go to the next host
-					if (ns.isRunning(pid, usableservers[i], usableserversthreadsavailable, target, hacktime) === false) {
-						continue;
+					if (weakenthreadsremaining <= maxthreadsonhost) {
+						ns.exec('/helpers/weaken1.js', usableservers[i].name, weakenthreadsremaining, target, 0);
+						weakenthreadsremaining = weakenthreadsremaining - maxthreadsonhost;
+						break threadloop;
+					} else if (weakenthreadsremaining > 0) {
+						ns.exec('/helpers/weaken1.js', usableservers[i].name, maxthreadsonhost, target, 0);
+						weakenthreadsremaining = weakenthreadsremaining - maxthreadsonhost;
 					}
 				}
-				firstweakenrunning = true;
 			}
+			sleeptime = ns.getWeakenTime(target);
+			await ns.sleep(sleeptime);
+		}
 
-			// grow
-			if (firstweakenrunning === true && growrunning === false && secondweakenrunning === false) {
-				usableserversthreadsavailable = 0;
+
+		// GROW
+		if (ns.getServerMoneyAvailable(target) < ns.getServerMaxMoney(target) && ns.getServerSecurityLevel(target) === ns.getServerMinSecurityLevel(target)) {
+			threadloop:
+			while (growthreadsremaining > 0) {
+				showstats(ns, target, usableservers, "Grow", growthreadsrequired, growthreadsremaining);
+				await ns.sleep(20);
+				usableserversloop:
 				for (let i = 0; i < usableservers.length; ++i) {
 					await ns.sleep(20);
-
-					// get usable ram and determine 
-					usableserversfreeram = ns.getServerMaxRam(usableservers[i]) - ns.getServerUsedRam(usableservers[i]);
-					usableserversthreadsavailable = Math.ceil(usableserversfreeram / growscriptram) - 2;
-					if (usableserversthreadsavailable === 0) {
-						continue;
+					let freeram = getfreeram(ns, usableservers[i].name);
+					if (freeram < growscriptram) {
+						continue usableserversloop;
 					}
-					if (remaininggrowthreads === 0) {
-						break;
-					}
+					let maxthreadsonhost = Math.floor(freeram / growscriptram);
 
-					// if remaining threads is less than the host can run, do the last exec. otherwise run with max available
-					// threads.
-					if (usableserversthreadsavailable > remaininggrowthreads) {
-						pid = ns.exec("/helpers/grow.js", usableservers[i], remaininggrowthreads, target, (weakentime + hacktime));
-						growtime = ns.getGrowTime(target) + sleepoffset;
-						//break;
+					if (growthreadsremaining <= maxthreadsonhost && growthreadsremaining > 0) {
+						ns.exec('/helpers/grow.js', usableservers[i].name, growthreadsremaining, target, 0);
+						growthreadsremaining = growthreadsremaining - maxthreadsonhost;
+						continue mainloop;
 					} else {
-						pid = ns.exec("/helpers/grow.js", usableservers[i], usableserversthreadsavailable, target, (weakentime + hacktime));
-						remaininggrowthreads = remaininggrowthreads - usableserversthreadsavailable;
-					}
-
-					// if batch is running go to the next host
-					if (ns.isRunning(pid, usableservers[i], usableserversthreadsavailable, target, (weakentime + hacktime)) === false) {
-						continue;
+						ns.exec('/helpers/grow.js', usableservers[i].name, maxthreadsonhost, target, 0);
+						growthreadsremaining = growthreadsremaining - maxthreadsonhost;
 					}
 				}
-				growrunning = true;
+				continue mainloop;  // this will be a problem when we can grow on less than max servers
 			}
+			sleeptime = ns.getWeakenTime(target);
+			await ns.sleep(sleeptime);
+		}
 
-			// second weaken
-			if (firstweakenrunning === true && growrunning === true && secondweakenrunning === false) {
-				usableserversthreadsavailable = 0;
+
+		// HACK
+		if (ns.getServerMoneyAvailable(target) === ns.getServerMaxMoney(target) && ns.getServerSecurityLevel(target) === ns.getServerMinSecurityLevel(target)) {
+			threadloop:
+			while (hackthreadsremaining > 0) {
+				showstats(ns, target, usableservers, "Hack", hackthreadsrequired, hackthreadsremaining);
+				await ns.sleep(20);
+				usableserversloop:
 				for (let i = 0; i < usableservers.length; ++i) {
 					await ns.sleep(20);
-
-					// get usable ram and determine 
-					usableserversfreeram = ns.getServerMaxRam(usableservers[i]) - ns.getServerUsedRam(usableservers[i]);
-					usableserversthreadsavailable = Math.ceil(usableserversfreeram / weakenscriptram) - 2;
-					if (usableserversthreadsavailable === 0) {
-						continue;
+					let freeram = getfreeram(ns, usableservers[i].name);
+					if (freeram < hackscriptram) {
+						continue usableserversloop;
 					}
-					if (remainingsecondweakenthreads === 0) {
-						break;
-					}
+					let maxthreadsonhost = Math.floor(freeram / hackscriptram);
 
-					// if remaining threads is less than the host can run, do the last exec. otherwise run with max available
-					// threads.
-					if (usableserversthreadsavailable > remainingsecondweakenthreads) {
-						pid = ns.exec("/helpers/weaken2.js", usableservers[i], remainingsecondweakenthreads, target, (weakentime + hacktime + growtime));
-						weakentime = ns.getWeakenTime(target) + sleepoffset;
-						//break;
+					if (hackthreadsremaining <= maxthreadsonhost && hackthreadsremaining > 0) {
+						ns.exec('/helpers/hack.js', usableservers[i].name, hackthreadsremaining, target, 0);
+						hackthreadsremaining = hackthreadsremaining - maxthreadsonhost;
+						continue mainloop;
 					} else {
-						pid = ns.exec("/helpers/weaken2.js", usableservers[i], usableserversthreadsavailable, target, (weakentime + hacktime + growtime));
-						remainingsecondweakenthreads = remainingsecondweakenthreads - usableserversthreadsavailable;
-					}
-
-					// if batch is running go to the next host
-					if (ns.isRunning(pid, usableservers[i], usableserversthreadsavailable, target, (weakentime + hacktime + growtime)) === false) {
-						continue;
+						ns.exec('/helpers/hack.js', usableservers[i].name, maxthreadsonhost, target, 0);
+						hackthreadsremaining = hackthreadsremaining - maxthreadsonhost;
 					}
 				}
-				secondweakenrunning = true;
+				continue mainloop;
 			}
-
-			ns.print("Big sleep for " + (weakentime + growtime + sleepoffset) + " ms");
-			await ns.sleep(weakentime + growtime + sleepoffset);
-			firstweakenrunning = false;
-			growrunning = false;
-			secondweakenrunning = false;
+			sleeptime = ns.getWeakenTime(target);
+			await ns.sleep(sleeptime);
 		}
 	}
-
-	ns.tprint("Script finished. This shouldnt happen.");
 }

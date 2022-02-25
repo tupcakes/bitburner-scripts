44442e53889540f19f68014204ad79672369ffdc
Mike Tupker <tupcakes@users.noreply.github.com>
2022-02-25 09:44:17 -0600

    fix

--- corps/hireemployees.js
+++ corps/hireemployees.js
@@ -11,10 +11,6 @@ export async function main(ns) {
 		let oneemployeecost = ns.corporation.getOfficeSizeUpgradeCost(division, city, 1);
 		let newemployeecount = Math.floor(ns.corporation.getCorporation().funds / oneemployeecost);
 
-		if (ns.corporation.getOffice(division, city).size > newemployeecount) {
-			continue;
-		}
-
 		ns.corporation.upgradeOfficeSize(division, city, newemployeecount);
 		for (let i = 0; i < newemployeecount; i++) {
 			ns.corporation.hireEmployee(division, city);
